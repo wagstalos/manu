b4727b0b67371eb7bc8c65f8f8a3533565736fa1
Wagner Paulo <wagstalos@hotmail.com>
2021-07-28 08:58:28 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var myCarousel = document.querySelector("#myCarousel");
+var carousel = new bootstrap.Carousel(myCarousel);
+
+// sticky
+function stickyElement(e) {
+  var header = document.querySelector(".default-header");
+  header.classList.toggle("sticky", window.scrollY > 10);
+}
+
+window.addEventListener("scroll", stickyElement);
+
+// $(document).ready(function () {
+//   $(".icon").click(function () {
+//     $(".icon").toggleClass("active");
+//   });
+// });
+
+function clickElement() {
+  var icon = document.querySelector(".icon");
+  icon.classList.toggle("active");
+  // mobile.classList.toggle("active");
+}
+
+window.addEventListener("click", clickElement);
+// window.addEventListener("touchstart", clickElement);
