f435f993d99d6d320a0a06ac27315c74eac6bda8
Wagner Paulo <wagstalos@hotmail.com>
2021-07-29 14:35:22 -0300

    swiper ajuste mobile

--- js/main.js
+++ js/main.js
@@ -7,6 +7,58 @@ function stickyElement(e) {
   header.classList.toggle("sticky", window.scrollY > 10);
 }
 
+var swiper2 = new Swiper(".mySwiper", {
+  spaceBetween: 20,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+  },
+});
+
+var swiper = new Swiper(".mySwiper2", {
+  spaceBetween: 20,
+  loop: false,
+  loopFillGroupWithBlank: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    400: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+});
+
 window.addEventListener("scroll", stickyElement);
 
 // $(document).ready(function () {
