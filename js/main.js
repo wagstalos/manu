a731a5f939c918494d7ef84de2416a34871fe394
Wagner Paulo <wagstalos@hotmail.com>
2021-08-03 12:46:52 -0300

    Página manu ok

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+AOS.init();
 // sticky
 function stickyElement(e) {
   var header = document.querySelector(".default-header");
@@ -98,3 +99,29 @@ $(document).ready(function () {
 
 // window.addEventListener("click", clickElement);
 // window.addEventListener("touchstart", clickElement);
+
+function calculaIdade(dataNasc) {
+  var dataAtual = new Date();
+  var anoAtual = dataAtual.getFullYear();
+  var anoNascParts = dataNasc.split("/");
+  var diaNasc = anoNascParts[0];
+  var mesNasc = anoNascParts[1];
+  var anoNasc = anoNascParts[2];
+  var idade = anoAtual - anoNasc;
+  var mesAtual = dataAtual.getMonth() + 1;
+  //Se mes atual for menor que o nascimento, nao fez aniversario ainda;
+  if (mesAtual < mesNasc) {
+    idade--;
+  } else {
+    //Se estiver no mes do nascimento, verificar o dia
+    if (mesAtual == mesNasc) {
+      if (new Date().getDate() < diaNasc) {
+        //Se a data atual for menor que o dia de nascimento ele ainda nao fez aniversario
+        idade--;
+      }
+    }
+  }
+  return idade;
+}
+// document.write(calculaIdade('25/03/1989'));
+document.getElementById("idade").innerHTML = calculaIdade("25/03/1993");
